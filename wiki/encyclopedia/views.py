db84fa1fbeb6d82f2c52d6069df0b3de3d3836e7
Bobsy2020 <robert.knops32@googlemail.com>
2020-07-12 14:20:59 +0100

    Render md to HTML

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+import markdown2
+from markdown2 import Markdown
 from . import util
+
 
 
 def index(request):
@@ -8,3 +11,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def title(request, title):
+
+    markdown_text = util.get_entry(title)
+    html = markdown2.markdown(markdown_text)
+    #markdowner = Markdown()
+    #html = HttpResponse(markdowner.convert(markdown_text))
+    return render(request, "encyclopedia/wiki.html", {
+        "text":html
+    })
